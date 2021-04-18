e2f4909557d6bcb34423a21f5a40e86898be60e7
Paras rana <52912280+paras1904@users.noreply.github.com>
2021-04-18 12:11:44 +0530

    Add files via upload

---
+++ 9 Naive bayes.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.naive_bayes import GaussianNB
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+# Assign colum names to the dataset
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+
+# Read dataset to pandas dataframe
+dataset = pd.read_csv(url, names=names)
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(y_pred)
