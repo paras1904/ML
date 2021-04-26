ae8800db78dfc78e36a3dcafc4da3baa24c94514
Paras rana <52912280+paras1904@users.noreply.github.com>
2021-04-27 04:00:52 +0530

    Add files via upload

---
+++ 14 Fuzzy c means.py
@@ -0,0 +1,23 @@
+import numpy as np
+from fcmeans import FCM
+from matplotlib import pyplot as plt
+
+n_samples = 3000
+
+X = np.concatenate((
+    np.random.normal((-2, -2), size=(n_samples, 2)),
+    np.random.normal((2, 2), size=(n_samples, 2))
+))
+
+fcm = FCM(n_clusters=2)
+fcm.fit(X)
+
+fcm_centers = fcm.centers
+fcm_labels = fcm.predict(X)
+
+# plot result
+f, axes = plt.subplots(1, 2, figsize=(11,5))
+axes[0].scatter(X[:,0], X[:,1], alpha=.1)
+axes[1].scatter(X[:,0], X[:,1], c=fcm_labels, alpha=.1)
+axes[1].scatter(fcm_centers[:,0], fcm_centers[:,1], marker="+", s=500, c='w')
+plt.show()
